adc4fa1ccd3417135e8c6f9375d6ce6f2063317b
Yusuke Wada <yusuke@kamawada.com>
2022-11-02 08:47:01 +0900

    commonjsやめた

--- webpack.common.js
+++ webpack.common.js
@@ -34,7 +34,6 @@ module.exports = {
               [
                 "@babel/preset-env",
                 {
-                  modules: "cjs",
                   spec: true,
                 },
               ],
