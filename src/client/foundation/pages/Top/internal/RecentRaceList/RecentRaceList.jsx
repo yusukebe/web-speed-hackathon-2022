17e1d96c1ea4d47131754e6c821a3c1ec647c22d
Yusuke Wada <yusuke@kamawada.com>
2022-11-02 09:24:41 +0900

    トップのraces画像を縮小、クロップ、webp化して直接読み込む

--- src/client/foundation/pages/Top/internal/RecentRaceList/RecentRaceList.jsx
+++ src/client/foundation/pages/Top/internal/RecentRaceList/RecentRaceList.jsx
@@ -1,28 +1,28 @@
-import React, { useEffect, useState } from "react";
-import styled from "styled-components";
+import React, { useEffect, useState } from "react"
+import styled from "styled-components"
 
-import { LinkButton } from "../../../../components/buttons/LinkButton";
-import { Spacer } from "../../../../components/layouts/Spacer";
-import { Stack } from "../../../../components/layouts/Stack";
-import { TrimmedImage } from "../../../../components/media/TrimmedImage";
-import { easeOutCubic, useAnimation } from "../../../../hooks/useAnimation";
-import { Color, FontSize, Radius, Space } from "../../../../styles/variables";
-import { formatCloseAt } from "../../../../utils/DateUtils";
+import { LinkButton } from "../../../../components/buttons/LinkButton"
+import { Spacer } from "../../../../components/layouts/Spacer"
+import { Stack } from "../../../../components/layouts/Stack"
+import { TrimmedImage } from "../../../../components/media/TrimmedImage"
+import { easeOutCubic, useAnimation } from "../../../../hooks/useAnimation"
+import { Color, FontSize, Radius, Space } from "../../../../styles/variables"
+import { formatCloseAt } from "../../../../utils/DateUtils"
 
 export const RecentRaceList = ({ children }) => {
   return (
     <Stack as="ul" gap={Space * 2}>
       {children}
     </Stack>
-  );
-};
+  )
+}
 
 const ItemWrapper = styled.li`
   background: ${Color.mono[0]};
   border-radius: ${Radius.MEDIUM};
   opacity: ${({ $opacity }) => $opacity};
   padding: ${Space * 3}px;
-`;
+`
 
 const RaceButton = styled(LinkButton)`
   background: ${Color.mono[700]};
@@ -33,12 +33,12 @@ const RaceButton = styled(LinkButton)`
   &:hover {
     background: ${Color.mono[800]};
   }
-`;
+`
 
 const RaceTitle = styled.h2`
   font-size: ${FontSize.LARGE};
   font-weight: bold;
-`;
+`
 
 /**
  * @typedef ItemProps
@@ -47,18 +47,18 @@ const RaceTitle = styled.h2`
 
 /** @type {React.VFC<ItemProps>} */
 const Item = ({ race }) => {
-  const [closeAtText, setCloseAtText] = useState(formatCloseAt(race.closeAt));
+  const [closeAtText, setCloseAtText] = useState(formatCloseAt(race.closeAt))
 
   // 締切はリアルタイムで表示したい
   useEffect(() => {
     const timer = setInterval(() => {
-      setCloseAtText(formatCloseAt(race.closeAt));
-    }, 0);
+      setCloseAtText(formatCloseAt(race.closeAt))
+    }, 0)
 
     return () => {
-      clearInterval(timer);
-    };
-  }, [race.closeAt]);
+      clearInterval(timer)
+    }
+  }, [race.closeAt])
 
   const {
     abortAnimation,
@@ -70,16 +70,20 @@ const Item = ({ race }) => {
     end: 1,
     start: 0,
     timingFunction: easeOutCubic,
-  });
+  })
 
   useEffect(() => {
-    resetAnimation();
-    startAnimation();
+    resetAnimation()
+    startAnimation()
 
     return () => {
-      abortAnimation();
-    };
-  }, [race.id, startAnimation, abortAnimation, resetAnimation]);
+      abortAnimation()
+    }
+  }, [race.id, startAnimation, abortAnimation, resetAnimation])
+
+  const match = race.image.match(/([0-9]+)\.jpg/)
+  const id = match[1]
+  const src = `/assets/images/races/100x100/${id}.webp`
 
   return (
     <ItemWrapper $opacity={opacity}>
@@ -93,12 +97,12 @@ const Item = ({ race }) => {
 
         <Stack.Item grow={0} shrink={0}>
           <Stack horizontal alignItems="center" gap={Space * 2}>
-            <TrimmedImage height={100} src={race.image} width={100} />
+            <img height={100} src={src} width={100} />
             <RaceButton to={`/races/${race.id}/race-card`}>投票</RaceButton>
           </Stack>
         </Stack.Item>
       </Stack>
     </ItemWrapper>
-  );
-};
-RecentRaceList.Item = Item;
+  )
+}
+RecentRaceList.Item = Item
