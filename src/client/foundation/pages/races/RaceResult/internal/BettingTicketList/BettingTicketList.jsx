9a852b6a60ca9056c94ac1087a7c34e612be6756
Yusuke Wada <yusuke@kamawada.com>
2022-11-03 17:11:37 +0900

    fontawesome 消す

--- src/client/foundation/pages/races/RaceResult/internal/BettingTicketList/BettingTicketList.jsx
+++ src/client/foundation/pages/races/RaceResult/internal/BettingTicketList/BettingTicketList.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import styled from "styled-components";
+import React from "react"
+import styled from "styled-components"
 
-import { EntryCombination } from "../../../../../components/displays/EntryCombination";
-import { Color, FontSize, Space } from "../../../../../styles/variables";
+import { EntryCombination } from "../../../../../components/displays/EntryCombination"
+import { Color, FontSize, Space } from "../../../../../styles/variables"
 
 const ItemWrapper = styled.tr`
   padding: ${Space * 1}px ${Space * 2}px;
@@ -10,12 +10,12 @@ const ItemWrapper = styled.tr`
   &:not(:last-child) {
     border-bottom: 1px solid ${Color.mono[400]};
   }
-`;
+`
 
 const Cell = styled.td`
   padding: ${Space * 1}px;
   text-align: ${({ $align }) => $align};
-`;
+`
 
 /**
  * @typedef ItemProps
@@ -32,18 +32,18 @@ const Item = ({ ticket: { key } }) => {
       </Cell>
       <Cell $align="right">100pt</Cell>
     </ItemWrapper>
-  );
-};
+  )
+}
 
 const Table = styled.table`
   border-spacing: 0;
-`;
+`
 
 const Header = styled.tr`
   th {
     border-bottom: 2px solid ${Color.mono[900]};
   }
-`;
+`
 
 const Placeholder = styled.div`
   align-items: center;
@@ -54,16 +54,19 @@ const Placeholder = styled.div`
   gap: ${Space * 2}px;
   justify-content: center;
   padding: ${Space * 2}px;
-`;
+`
 
 export const BettingTicketList = ({ children }) => {
   if (React.Children.count(children) === 0) {
     return (
       <Placeholder>
-        <i className="fas fa-ticket-alt" />
+        <svg aria-hidden="true" data-fa-i2svg="" data-icon="ticket-alt" data-prefix="fas" focusable="false" role="img" style={{
+          height: '1em', verticalAlign:
+            '-0.125em', width: '1.125em'
+        }} viewBox="0 0 576 512" xmlns="http://www.w3.org/2000/svg"><path d="M128 160h320v192H128V160zm400 96c0 26.51 21.49 48 48 48v96c0 26.51-21.49 48-48 48H48c-26.51 0-48-21.49-48-48v-96c26.51 0 48-21.49 48-48s-21.49-48-48-48v-96c0-26.51 21.49-48 48-48h480c26.51 0 48 21.49 48 48v96c-26.51 0-48 21.49-48 48zm-48-104c0-13.255-10.745-24-24-24H120c-13.255 0-24 10.745-24 24v208c0 13.255 10.745 24 24 24h336c13.255 0 24-10.745 24-24V152z" fill="currentColor"></path></svg>
         <div>購入した拳券はありません</div>
       </Placeholder>
-    );
+    )
   }
 
   return (
@@ -79,6 +82,6 @@ export const BettingTicketList = ({ children }) => {
       </thead>
       <tbody>{children}</tbody>
     </Table>
-  );
-};
-BettingTicketList.Item = Item;
+  )
+}
+BettingTicketList.Item = Item
