34479fd8661c7d27c96f2bb741ebe8047e597cb7
Yusuke Wada <yusuke@kamawada.com>
2022-11-06 18:23:19 +0900

    footer をsuspense

--- src/client/foundation/layouts/CommonLayout/CommonLayout.jsx
+++ src/client/foundation/layouts/CommonLayout/CommonLayout.jsx
@@ -1,18 +1,21 @@
-import React from "react"
+import React, { lazy, Suspense } from "react"
 import { Outlet } from "react-router-dom"
 
-import Footer from "../../components/navs/Footer"
+//import Footer from "../../components/navs/Footer"
 import { Header } from "../../components/navs/Header/Header"
+
+const Footer = lazy(() => import("../../components/navs/Footer"))
 
 export const CommonLayout = () => {
   return (
     <div>
       <Header />
-
       <main>
         <Outlet />
       </main>
-      <Footer />
+      <Suspense fallback="loading...">
+        <Footer />
+      </Suspense>
     </div>
   )
 }
