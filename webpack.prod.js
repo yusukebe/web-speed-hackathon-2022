4767e2dfb189f4784f6ed40316995ccd77972822
Yusuke Wada <yusuke@kamawada.com>
2022-11-06 14:24:40 +0900

    tarser plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+const TerserPlugin = require("terser-webpack-plugin");
 const { merge } = require("webpack-merge"); // webpack-merge
 
 const common = require("./webpack.common.js"); // 汎用設定をインポート
@@ -6,4 +7,8 @@ const common = require("./webpack.common.js"); // 汎用設定をインポート
 // common設定とマージする
 module.exports = merge(common, {
   mode: "production", // 開発モード
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
 });
