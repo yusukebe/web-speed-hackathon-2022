b76b83f01f1dbca828f08447c8952d11c04c018c
Yusuke Wada <yusuke@kamawada.com>
2022-11-14 23:55:27 +0900

    suspenseをoutletに

--- src/client/foundation/layouts/CommonLayout/CommonLayout.jsx
+++ src/client/foundation/layouts/CommonLayout/CommonLayout.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/display-name */
-import React from "react"
+import React, { Suspense } from "react"
 import { Outlet } from "react-router-dom"
 
 import Footer from "../../components/navs/Footer"
@@ -11,7 +11,9 @@ export const CommonLayout = React.memo(() => {
     <div>
       <Header />
       <main>
-        <Outlet key="outlet" />
+        <Suspense fallback="loading...">
+          <Outlet key="outlet" />
+        </Suspense>
       </main>
       <Footer />
     </div>
