f2dd772e6a636eb762388c5446a7f5187310d506
Yusuke Wada <yusuke@kamawada.com>
2022-11-15 00:02:49 +0900

    fix preload

--- src/server/routes/app.jsx
+++ src/server/routes/app.jsx
@@ -67,12 +67,13 @@ export const appRoute = async (fastify) => {
     const match = race.image.match(/([0-9]+)\.jpg$/)
     const imageURL = `/assets/images/races/400x225/${match[1]}.webp`
 
-    let hero = `<link rel="preload" href="/assets/js/main.bundle.js" as="script" /><link rel="preload" href="${imageURL}" as="image" />` // `<link rel="preload" href="${grayURL}" as="image" />`
+    let hero = `<link rel="preload" href="${imageURL}" as="image" />`
 
     if (req.url.toString().match(/.+odds$/)) {
-      res.raw.setHeader("Link", `</assets/js/main.bundle.js>; rel=preload; as=script, <${imageURL}>; rel=preload; as=image, </assets/fonts/MODI_Senobi-Gothic_2017_0702/Senobi-Gothic-Bold.woff>; rel=preload; as=font; crossorigin`)
+      hero = hero + `<link rel="preload" href="/assets/fonts/MODI_Senobi-Gothic_2017_0702/Senobi-Gothic-Bold.woff" as="font" />`
+      res.raw.setHeader("Link", `<${imageURL}>; rel=preload; as=image, </assets/fonts/MODI_Senobi-Gothic_2017_0702/Senobi-Gothic-Bold.woff>; rel=preload; as=font; crossorigin`)
     } else {
-      res.raw.setHeader("Link", `</assets/js/main.bundle.js>; rel=preload; as=script, <${imageURL}>; rel=preload; as=image`)
+      res.raw.setHeader("Link", `<${imageURL}>; rel=preload; as=image`)
     }
 
     res.raw.setHeader("Content-Type", "text/html; charset=utf-8")
